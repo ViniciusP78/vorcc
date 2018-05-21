da5f494fa771762cc64e4900c8a98081857b02a5
Vinicius0078 <vinicius0078@gmail.com>
2018-05-21 10:38:59 -0300

    Adição da página login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title> Vorcc </title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+    </head>
+    <body>
+        <form method="post" class="login-container">
+            <input type="text" placeholder="Login" name="login"><br>
+            <input type="password" placeholder="Senha" name="senha"><br>
+            <input type="submit" value="Entrar" name="submit">
+        </form>
+
+
+        </div>
+    </body>
+</html>
