0a90543c821bb33dd04b30305df9d1e4a191b960
Vinicius0078 <vinicius0078@gmail.com>
2018-05-28 12:16:27 -0300

    Adicionado cadastro com PIN

---
+++ cadastro.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title> Vorcc </title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+    </head>
+    <body>
+        <?php
+        function generateRandomString($length = 32) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*+';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+        ?>
+        <form method="post" class="login-container" action="">
+            Sou: Comprador<input type="radio" name="bool_fornecedor" value="0"> Fornecedor<input type="radio" name="bool_fornecedor" value="1">
+            <input type="text" placeholder="Nome Real" name="nome"><br>
+            <input type="number" placeholder="CPF (somente números)" name="cpf"><br>
+            <input type="text" placeholder="Login" name="login"><br>
+            <input type="password" placeholder="Senha" name="senha"><br>
+            <input type="password" placeholder="Repita a Senha" name="senha2"><br>
+            Já tem uma empresa cadastrada ? <br>
+            Sim<input type="radio" name="empcads" value="0" onclick="switchs()">
+            Não<input type="radio" name="empcads" value="1" onclick="switchs()">
+            <div id="empresa">
+                <input type="text" placeholder="Nome da empresa" name="nome_empresa"><br>
+                <input type="number" placeholder="CNPJ (somente números)" name="cnpj"><br>
+            </div>
+            <div id="pin">
+                <input type="text" placeholder="PIN da empresa" name="pin"><br>
+            </div>
+            <input type="submit" value="Entrar" name="submit">
+        </form>
+
+        <script>
+            function switchs() {
+                var radios = document.getElementsByName('empcads');
+                var switchs;
+
+                for (var i = 0, length = radios.length; i < length; i++)
+                {
+                    if (radios[i].checked)
+                    {
+                        // do whatever you want with the checked radio
+                        switchs = radios[i].value;
+
+                        break;
+                    }
+                }
+
+                if (switchs == 0) {
+                    document.getElementById('empresa').style.display = "none";
+                    document.getElementById('pin').style.display = "block";
+
+                } else {
+                    document.getElementById('empresa').style.display = "block";
+                    document.getElementById('pin').style.display = "none";
+                }}
+        </script>
+
+        <?php
+
+        if(isset($_POST['nome']) && isset($_POST['bool_fornecedor']) && isset($_POST['cpf']) && isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['senha2']) && isset($_POST['empcads'])){
+            echo '3';
+            if($_POST['empcads'] == 1){
+                echo '2';
+                if(isset($_POST['nome_empresa']) && isset($_POST['cnpj'])){
+                    echo '1';
+                    include('php/conexao.php');
+                    $query = $conn->prepare("INSERT INTO tb_empresa VALUES(null, :nome, :cnpj, DEFAULT, :fornecedor, :pin)");
+                    $query->bindValue(":nome", $_POST['nome_empresa']);
+                    $query->bindValue(":cnpj", $_POST['cnpj']);
+                    $query->bindValue(":fornecedor", $_POST['bool_fornecedor']);
+                    $query->bindValue(":pin", generateRandomString());
+                    $query->execute();
+
+                    $query = $conn->prepare("SELECT cd_empresa FROM tb_empresa WHERE nr_cnpj = :cnpj");
+                    $query->bindValue(":cnpj", $_POST['cnpj']);
+                    $query->execute();
+
+                    $cd_empresa = "";
+                    while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                        $cd_empresa = $row['cd_empresa'];
+                        echo 'roooow';
+                    }
+
+                    $query = $conn->prepare("INSERT INTO dono VALUES(null, :nome, :cpf, :login, :senha, :cd_empresa)");
+                    $query->bindValue(":nome", $_POST['nome']);
+                    $query->bindValue(":cpf", $_POST['cpf']);
+                    $query->bindValue(":login", $_POST['login']);
+                    $query->bindValue(":senha", md5($_POST['senha']));
+                    $query->bindValue(":cd_empresa", $cd_empresa);
+                    $query->execute();
+
+                    /* SELECIONANDO O ID DA EMPRESA PRA FAZER O LOGIN E REDIRECIONAR PRA DASHBOARD */
+                    $query = $conn->prepare("SELECT id_empresa, cd_usuario FROM dono WHERE nr_cpf = :cpf");
+                    $query->bindValue(":cpf", $_POST['cpf']);
+                    $query->execute();
+
+                    while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                        $_SESSION['nome'] = $_POST['nome'];
+                        $_SESSION['cd_usuario'] = $row['cd_usuario'];
+                        $_SESSION['id_empresa'] = $row['id_empresa'];
+                        echo $_SESSION['nome'].' '.$_SESSION['cd_usuario'].' '.$_SESSION['id_empresa'];
+                    }
+                    /*header("Location: home.php");*/
+                }
+            }
+            else{
+                include('php/conexao.php');
+                $query = $conn->prepare("SELECT * FROM tb_empresa WHERE vl_pin = :pin");
+                $query->bindValue(":pin", $_POST['pin']);
+                $query->execute();
+
+                while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                    $cd_empresa = $row['cd_empresa'];
+                }
+
+                $query = $conn->prepare("INSERT INTO dono VALUES(null, :nome, :cpf, :login, :senha, :cd_empresa)");
+                $query->bindValue(":nome", $_POST['nome']);
+                $query->bindValue(":cpf", $_POST['cpf']);
+                $query->bindValue(":login", $_POST['login']);
+                $query->bindValue(":senha", md5($_POST['senha']));
+                $query->bindValue(":cd_empresa", $cd_empresa);
+                $query->execute();
+
+                /* SELECIONANDO O ID DA EMPRESA PRA FAZER O LOGIN E REDIRECIONAR PRA DASHBOARD */
+                $query = $conn->prepare("SELECT id_empresa, cd_usuario FROM dono WHERE cpf = :cpf");
+                $query->bindValue(":cpf", $_POST['cpf']);
+                $query->execute();
+
+                while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                    session_start();
+                    $_SESSION['nome'] = $_POST['nome'];
+                    $_SESSION['cd_usuario'] = $row['cd_usuario'];
+                    $_SESSION['id_empresa'] = $row['id_empresa'];
+                }
+                header("Location: home.php");
+            }
+        }
+        ?>
+
+        </div>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php include('php/conexao.php'); ?>
 <!doctype html>
 <html>
     <head>
@@ -11,11 +12,36 @@
     </head>
     <body>
         <form method="post" class="login-container">
-            <input type="text" placeholder="Login" name="login"><br>
-            <input type="password" placeholder="Senha" name="senha"><br>
-            <input type="submit" value="Entrar" name="submit">
+            <div id="title">Vorcc</div>
+            <div id="form-container">
+                <input type="text" placeholder="Login" name="login" class="form-input" required><div class="input-mask"></div><br>
+                <input type="password" placeholder="Senha" name="senha" class="form-input" required><div class="input-mask"></div><br>
+                <input type="submit" value="Entrar" name="submit">
+            </div>
         </form>
 
+        <?php
+            if(isset($_SESSION['nome'])){
+                header("Location: home.php");
+            }
+            if(isset($_POST['login'])){
+                $query = $conn->prepare("SELECT * FROM dono WHERE nm_senha = :senha AND nm_login = :login");
+                $query->bindValue(':senha', md5($_POST['senha']));
+                $query->bindValue(':login', $_POST['login']);
+                $query->execute();
+
+                if($query->rowCount() == 1){
+                    while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                        $_SESSION['id_empresa'] = $row['id_empresa'];
+                        $_SESSION['cd_dono'] = $row['cd_usuario'];
+                        $_SESSION['nome'] = $row['nm_usuario'];
+                        header("Location: home.php");
+                    }
+                }else{
+                    echo '??';
+                }
+            }
+        ?>
 
         </div>
     </body>
